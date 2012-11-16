f2e75572409dbd5cdc548763e7f210157455caed
Dmitriy Vinogradov <optofile@gmail.com>
2012-11-16 17:49:24 +0400

    Version number has been moved to module namespace

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__docformat__ = "restructuredtext en"
+
+
+# Don't to forget to update Sphinx configuration as well.
+# TODO: find a way to do this automatically.
+__version__ = "2012.2"
