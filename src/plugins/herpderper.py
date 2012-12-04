cd63052b5b7f51b9ba59ecaa1414e1a701fda958
Dmitriy Vinogradov <optofile@gmail.com>
2012-12-04 08:38:46 +0400

    Introducing yet another useless plugin

---
+++ src/plugins/herpderper.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+:mod:`herpderper` --- Derping the herp since 1895
+=================================================
+"""
+
+__docformat__ = "restructuredtext en"
+
+
+import random
+
+from Skype4Py.enums import cmsReceived
+
+from plugin import Plugin
+
+
+class HerpDerper(Plugin):
+    def __init__(self, parent):
+        super(HerpDerper, self).__init__(parent)
+
+    def on_message_status(self, message, status):
+        if status != cmsReceived:
+            return
+
+        strings = [
+            u"губи",
+            u"gooby",
+            u"uemb",
+            u"пщщин",
+            u"губе",
+            u"ue,t",
+        ]
+
+        if not any(s in message.Body.lower() for s in strings):
+            return
+
+        chat = message.Chat
+
+        max_words_count = 5
+
+        words_count = random.randint(1, max_words_count)
+
+        derps = ("derp " * words_count).split()
+        herps = ("herp " * (max_words_count - words_count)).split()
+
+        output = derps + herps
+        random.shuffle(output)
+
+        chat.SendMessage(" ".join(output))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
