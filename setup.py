c8bbc6248edbd244a8a2a8c9b835ed26fdd3dcc8
Dmitriy Vinogradov <optofile@gmail.com>
2013-02-13 17:09:00 +0400

    Refactoring

--- setup.py
+++ setup.py
@@ -14,43 +14,32 @@ for line in open(os.path.abspath("./src/__init__.py")):
         break
 
 
-params = {
-    "name": "gooby",
-    "version": __version__,
-    "description": "A modular Skype bot",
-    "author": "Dmitriy Vinogradov",
-    "author_email": "tetra5dotorg@gmail.com",
-    "url": "https://github.com/tetra5/gooby",
-    "keywords": "python skype skype4py bot",
-    "license": "MIT",
-    "platforms": ["Windows", "Linux", "MacOS X"],
-    "requires": ["Skype4Py (>=1.0.32)"],
-    "provides": ["gooby"],
-    "long_description" : """Gooby is a modular plugin-based Skype bot built on
-        top of Skype4Py Skype API wrapper written completely in Python.""",
-
-    "packages": ["gooby", "gooby.plugins"],
-
-    "package_dir": {
-        "gooby": "src",
-        "gooby.plugins": "src/plugins",
-        },
-
-    "classifiers": [
-        "Development Status :: Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Topic :: Utilities",
-        "Intended Audience :: Developers",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS :: MacOS X",
-        "Programming Language :: Python :: 2",
-        ],
-    }
-
-
 if __name__ == "__main__":
-    setup(**params)
+    setup(name="gooby",
+          version=__version__,
+          description="A modular Skype bot",
+          author="Dmitriy Vinogradov",
+          author_email="tetra5dotorg@gmail.com",
+          url="https://github.com/tetra5/gooby",
+          keywords="python skype skype4py bot",
+          license="MIT",
+          platforms=["Windows", "Linux", "MacOS X"],
+          requires=["Skype4Py (>=1.0.32)", "lxml (>=2.3.2)"],
+          provides=["gooby"],
+          long_description="""Gooby is a modular plugin-based Skype bot built
+on top of Skype4Py Skype API wrapper written completely in Python.""",
+          packages=["gooby", "gooby.plugins"],
+          package_dir={"gooby": "src",
+                       "gooby.plugins": "src/plugins"},
+          classifiers=["Development Status :: Development Status :: 4 - Beta",
+                       "Environment :: Console",
+                       "Topic :: Utilities",
+                       "Intended Audience :: Developers",
+                       "Intended Audience :: End Users/Desktop",
+                       "License :: OSI Approved :: MIT License",
+                       "Natural Language :: English",
+                       "Operating System :: Microsoft :: Windows",
+                       "Operating System :: POSIX :: Linux",
+                       "Operating System :: MacOS :: MacOS X",
+                       "Programming Language :: Python :: 2"],
+          )
