31e0ffc61ac312894a63c86919dd59cf7ed8f922
Dmitriy Vinogradov <optofile@gmail.com>
2013-07-05 00:41:44 +0400

    Changed version number

--- src/__init__.py
+++ src/__init__.py
@@ -8,4 +8,24 @@ __docformat__ = "restructuredtext en"
 __author__ = "Dmitriy Vinogradov <tetra5dotorg@gmail.com>"
 
 
-__version__ = "2012.3"
+__version__ = "2013.1"
+
+
+# Changelog.
+#
+# 2013.1
+# ======
+# * Cache system
+# * Plugin system
+# * New plugins:
+#     - EzRoller
+#     - HerpDerper
+#     - IMDbURLParser
+#     - MarkAsRead
+#     - NonceGenerator
+#     - Poll
+#     - SteamURLParser
+#     - URLDiscoverer
+#     - VimeoURLParser
+#     - WeatherForecast
+#     - YouTubeURLParser
