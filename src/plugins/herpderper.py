2e4e553d481ae99e5498da4488f92dc6e376187c
Dmitriy Vinogradov <optofile@gmail.com>
2013-07-11 03:43:35 +0400

    Bug fixes, refactoring; quoted messages are now getting skipped; jackpot winners statistics is now maintained via cache

--- src/plugins/herpderper.py
+++ src/plugins/herpderper.py
@@ -12,6 +12,7 @@ __docformat__ = "restructuredtext en"
 
 
 import random
+import re
 
 from Skype4Py.enums import cmsReceived, cmeEmoted
 
@@ -21,9 +22,6 @@ from plugin import Plugin
 def all_same(myiter):
     """
     Check whether all elements in iterable are the same.
-
-    :return: True or False
-    :rtype: `bool`
 
     >>> all_same([1, 1, 1, 1])
     True
@@ -38,7 +36,141 @@ def all_same(myiter):
     return all([myiter[0] == item for item in myiter])
 
 
-# TODO: Maintain jackpot statistics.
+def parse_linux_quote(message):
+    """
+    >>> message = '''
+    ... > [Wednesday, June 26, 2013 1:37:59 PM username derp] a quoted message
+    ...
+    ... some other message.
+    ... '''
+    >>> found = parse_linux_quote(message)
+    >>> assert found is not None
+    >>> assert found.group("quote") == "a quoted message"
+    """
+
+    pattern = re.compile(
+        r"""
+        # Conversation message pattern which matches Linux Skype client message
+        # quotation.
+        >\s\[
+        (?P<datetime>
+            (?P<day>
+                \w+
+            ),\s
+            (?P<month>
+                \w+
+            )\s
+            (?P<date>
+                \d{1,2}
+            ),\s
+            (?P<year>
+                \d{4}
+            )\s
+            (?P<time>
+                \d{1,2}:\d{2}:\d{2}
+            )\s
+            (?P<ampm>
+                \w{2}
+            )
+        )\s
+        (?P<sender>
+            .*
+        )
+        ]\s
+        (?P<quote>
+            .*
+        )\n?
+        (?P<message>
+            .*
+        )
+        """,
+        re.UNICODE | re.VERBOSE)
+
+    return re.search(pattern, message)
+
+
+def parse_macosx_quote(message):
+    """
+    >>> message = '''
+    ... 26.06.13 в 13:37 skypename.derp написал (-а):
+    ... > a quoted message
+    ... some other message.
+    ... '''
+    >>> found = parse_macosx_quote(message)
+    >>> assert found is not None
+    >>> assert found.group("quote") == "a quoted message"
+    """
+
+    pattern = re.compile(
+        r"""
+        # Conversation message pattern which matches Mac OS X Skype client
+        # message quotation.
+        (?P<date>
+            \d{2}.\d{2}.\d{2}
+        )\s\w+\s
+        (?P<time>
+            \d{2}:\d{2}
+        )\s
+        # There are no space characters in Skype user names.
+        (?P<sender>
+            [a-zA-Z0-9_\-\.]+
+        )\s.*:\n
+        >\s?
+        (?P<quote>
+            .*
+        )\n?
+        (?P<message>
+            .*
+        )
+        """,
+        re.UNICODE | re.VERBOSE)
+
+    return re.search(pattern, message)
+
+
+def parse_windows_quote(message):
+    """
+    >>> message = '''
+    ... [1:37:59 PM] username derp: a quoted message
+    ...
+    ... <<< some other message.
+    ... '''
+    >>> found = parse_windows_quote(message)
+    >>> assert found is not None
+    >>> assert found.group("quote") == "a quoted message"
+    """
+
+    pattern = re.compile(
+        r"""
+        # Conversation message pattern which matches Windows Skype client
+        # message quotation.
+        \[
+        (?P<time>
+            \d{1,2}:\d{2}:\d{2}
+        )\s
+        (?P<ampm>
+            \w{2}
+        )]\s
+        (?P<sender>
+            .*
+        ):\s
+        (?P<quote>
+            .*
+        )\n*
+        <<<\s
+        (?P<message>
+            .*
+        )
+        """,
+        re.UNICODE | re.VERBOSE)
+
+    return re.search(pattern, message)
+
+
+def message_is_quoted(message):
+    return parse_linux_quote(message) is None \
+        and parse_macosx_quote(message) is None \
+        and parse_windows_quote(message) is None
 
 
 class HerpDerper(Plugin):
@@ -59,21 +191,34 @@ class HerpDerper(Plugin):
         if status != cmsReceived or message.Type == cmeEmoted:
             return
 
+        if message_is_quoted(message.Body.strip()):
+            return
+
         if not any(t.lower() in message.Body.lower() for t in self._triggers):
             return
 
         msg = []
 
-        max_herpderps = 4
-        words_count = random.randint(1, max_herpderps)
-        herps = ["herp"] * random.randint(1, words_count)
-        herpsderps = herps + ["derp"] * (words_count - len(herps))
-        random.shuffle(herpsderps)
+        max_words = 3
+        words = []
+        for _ in xrange(1, max_words + 1):
+            words.append(random.choice(["herp", "derp", "hurr", "durr"]))
 
-        msg.append(" ".join(herpsderps))
+        msg.append(" ".join(words))
 
-        if len(herpsderps) is max_herpderps and all_same(herpsderps):
-            msg.append("{0} wins the jackpot!".format(message.FromDisplayName))
+        if len(words) is max_words and all_same(words):
+            self._logger.info("Jackpot winner {0}".format(message.FromHandle))
+
+            try:
+                wins = int(self._cache.get(message.FromHandle))
+            except TypeError:
+                wins = 0
+            wins += 1
+            self._cache.set(message.FromHandle, wins)
+
+            msg.append("{0} wins the jackpot! {1} in total".format(
+                message.FromDisplayName, wins
+            ))
 
         message.Chat.SendMessage(u"\n".join(msg))
 
