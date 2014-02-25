9ebacb6e898558230c9c46f6232e021893c9b501
Dmitriy Vinogradov <optofile@gmail.com>
2014-02-26 03:49:59 +0400

    Derp

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+:mod:``
+=======
+"""
+
+from __future__ import unicode_literals
+
+
+__docformat__ = "restructuredtext en"
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
