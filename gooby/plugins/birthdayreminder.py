979baea9b8cdc25d399869bbd528ced7ca94a501
Dmitriy Vinogradov <optofile@gmail.com>
2014-03-07 13:43:09 +0400

    Added "!birthdays" chat command

--- gooby/plugins/birthdayreminder.py
+++ gooby/plugins/birthdayreminder.py
@@ -17,10 +17,20 @@ __docformat__ = "restructuredtext en"
 import datetime
 from threading import Timer
 import random
+import operator
+
+from Skype4Py.enums import cmsReceived
 
 from plugin import Plugin
 from output import ChatMessage
 
+
+NOTIFICATION_TEMPLATE = "Today is {recipient} birthday! {interjection}"
+
+REMINDER_TEMPLATE = """
+It was {last_recipient} birthday {last_delta} {last_day_or_days} ago.
+{next_recipient} birthday is coming up next in {next_delta} {next_day_or_days}.
+"""
 
 INTERJECTIONS = [
     "Hurrah!",
@@ -30,7 +40,7 @@ INTERJECTIONS = [
     "Awesome!",
     "Whee!",
     "Whoa!",
-    "Unbelievable!"
+    "Unbelievable!",
     "Wow!",
     "Amazing!",
     "Yahoo!",
@@ -40,6 +50,7 @@ INTERJECTIONS = [
     "Exciting!",
     "Yee-haw!",
     "Sweet!",
+    "Fantastic!",
 ]
 
 
@@ -79,6 +90,28 @@ def str_to_datetime(date_str):
     if dt is None:
         raise ValueError("Input string does not match any known date format")
     return dt
+
+
+def _format_recipients(recipients):
+    """
+    >>> _format_recipients(["herp"])
+    u"herp's"
+
+    >>> _format_recipients(["herp", "derp"])
+    u"herp's and derp's"
+
+    >>> _format_recipients(["herp", "derp", "durp"])
+    u"herp's, derp's and durp's"
+    """
+
+    if not recipients:
+        raise ValueError("Input list should contain at least one string")
+
+    recs = ["{0}'s".format(recipient) for recipient in recipients[:]]
+    if len(recs) is 1:
+        return recs[0]
+    elif len(recs) > 1:
+        return "{0} and {1}".format(", ".join(recs[:-1]), recs[-1])
 
 
 class BirthdayReminder(Plugin):
@@ -127,23 +160,57 @@ class BirthdayReminder(Plugin):
         self._timer.start()
 
     def _notify(self, recipients):
-        if not recipients:
-            raise ValueError("Input list should contain at least one string")
-
-        template = "Today is {0} birthday!"
-        message = None
-
-        recs = ["{0}'s".format(recipient) for recipient in recipients[:]]
-        if len(recs) is 1:
-            message = template.format(recs[0])
-        elif len(recs) > 1:
-            m = "{0} and {1}".format(", ".join(recs[:-1]), recs[-1])
-            message = template.format(m)
-
-        message = "{0} {1}".format(message, random.choice(INTERJECTIONS))
+        recs = _format_recipients(recipients)
+        substitutes = {
+            "recipient": recs,
+            "interjection": random.choice(INTERJECTIONS),
+        }
+        msg = NOTIFICATION_TEMPLATE.format(**substitutes)
 
         for chat in self.whitelist:
-            self.output.append(ChatMessage(chat, message))
+            self.output.append(ChatMessage(chat, msg))
+
+    def on_message_status(self, message, status):
+        if status != cmsReceived:
+            return
+
+        if not message.Body.startswith("!birthdays"):
+            return
+
+        _pluralize = lambda n, s: "{0}s".format(s) if n > 1 else s
+
+        last_deltas = list()
+        next_deltas = list()
+        today = datetime.datetime.today()
+        for name, dt in self.birthdays.iteritems():
+            dt = dt.replace(year=today.year)
+            delta = today - dt
+            item = (name, delta)
+            if delta.days > 0:
+                last_deltas.append(item)
+            elif delta.days < 0:
+                next_deltas.append(item)
+
+        last_item = min(last_deltas, key=operator.itemgetter(1))
+        last_recipient = _format_recipients((last_item[0],))
+        last_delta = last_item[1].days
+        next_item = max(next_deltas, key=operator.itemgetter(1))
+        next_recipient = _format_recipients((next_item[0],))
+        next_delta = abs(next_item[1].days)
+        substitutes = {
+            "last_recipient": last_recipient,
+            "last_delta": last_delta,
+            "last_day_or_days": _pluralize(last_delta, "day"),
+            "next_recipient": next_recipient,
+            "next_delta": next_delta,
+            "next_day_or_days": _pluralize(next_delta, "day"),
+        }
+        msg = REMINDER_TEMPLATE.format(**substitutes)
+
+        self.output.append(ChatMessage(message.Chat.Name, msg))
+
+        return message, status
+
 
 if __name__ == "__main__":
     import doctest
