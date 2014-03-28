1ee3e5903218851842edc4c936c818929cc21eba
Dmitriy Vinogradov <optofile@gmail.com>
2014-03-28 09:42:46 +0400

    Initial commit

---
+++ gooby/signals.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+:mod:`signals` --- Internal signals collection
+==============================================
+"""
+
+
+from __future__ import unicode_literals
+
+
+__docformat__ = "restructuredtext en"
+
+
+REQUEST_USAGE = "request_plugin_usage"
+REQUEST_PLUGINS = "request_plugins_list"
+REQUEST_CHATS = "request_chats_list"
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
