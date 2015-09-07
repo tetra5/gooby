ba57046cd8a2b990108b34eeb768686309636143
Dmitriy Vinogradov <optofile@gmail.com>
2015-09-07 12:32:00 +0300

    Expiration delta tweak

--- gooby/plugins/summarygenerator.py
+++ gooby/plugins/summarygenerator.py
@@ -322,7 +322,7 @@ class SummaryGenerator(Plugin):
     # n messages received.
     TRIGGER_THRESHOLD = 150
 
-    EXPIRATION_TIMEDELTA = timedelta(days=14)
+    EXPIRATION_TIMEDELTA = timedelta(weeks=4)
 
     def _init_cache(self):
         return from_dict({
